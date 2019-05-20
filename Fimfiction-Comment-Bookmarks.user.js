c8b1df9220677204e24bd123ca6e83c12997f41d
Mark Taiwan <mark.taiwan@gmail.com>
2019-05-21 00:03:04 +0800

    Version bump to 1.0.7

--- Fimfiction-Comment-Bookmarks.user.js
+++ Fimfiction-Comment-Bookmarks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fimfiction Comment Bookmarks
 // @description  Comment bookmarking on Fimfiction.net
-// @version      1.0.6
+// @version      1.0.7
 // @author       Marker
 // @license      MIT
 // @namespace    https://github.com/marktaiwan/
